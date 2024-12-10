204f5f0375ce9f1cffa29561a04a334eeda5448e
Enkuryo <info@enkuryo.eu>
2024-12-10 23:37:35 +0100

    Add solution for 2024 day 10 part 2

--- src/2024/Day 10/day10.test.ts
+++ src/2024/Day 10/day10.test.ts
@@ -4,7 +4,7 @@ import { partOne, partTwo } from './day10';
 
 const EXAMPLE_INPUT = readFile(__dirname + '/example.txt');
 
-const TESTS: Array<{ expected: number; input: string[] }> = [
+const TESTS_PART_ONE: Array<{ expected: number; input: string[] }> = [
   {
     expected: 2,
     input: `...0...
@@ -37,16 +37,64 @@ const TESTS: Array<{ expected: number; input: string[] }> = [
   },
 ];
 
+const TESTS_PART_TWO: Array<{ expected: number; input: string[] }> = [
+  {
+    expected: 3,
+    input: `.....0.
+..4321.
+..5..2.
+..6543.
+..7..4.
+..8765.
+..9....`.split('\n'),
+  },
+  {
+    expected: 13,
+    input: `..90..9
+...1.98
+...2..7
+6543456
+765.987
+876....
+987....`.split('\n'),
+  },
+  {
+    expected: 227,
+    input: `012345
+123456
+234567
+345678
+4.6789
+56789.`.split('\n'),
+  },
+  {
+    expected: 81,
+    input: `89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732`.split('\n'),
+  },
+];
+
 describe('Year 2024 - Day 10', () => {
-  for (const testCase of TESTS) {
+  for (const testCase of TESTS_PART_ONE) {
     test('Part 1 - Expect test to return ' + testCase.expected, () => {
       expect(partOne(testCase.input)).toBe(testCase.expected);
     });
   }
-
   test('Part 1 - Example Input returns 36', () => {
     expect(partOne(EXAMPLE_INPUT)).toBe(36);
   });
+
+  for (const testCase of TESTS_PART_TWO) {
+    test('Part 2 - Expect test to return ' + testCase.expected, () => {
+      expect(partTwo(testCase.input)).toBe(testCase.expected);
+    });
+  }
 
   /*test('Part 2 - Example Input returns 1', () => {
     expect(partTwo(EXAMPLE_INPUT)).toBe(1);
--- src/2024/Day 10/day10.ts
+++ src/2024/Day 10/day10.ts
@@ -4,6 +4,8 @@ export const PUZZLE_INPUT = readFile(__dirname + '/input.txt');
 
 type MapType = Array<string[]>;
 
+type PathType = Array<string>;
+
 type Point = { x: number; y: number };
 
 const DIRECTIONS: Point[] = [
@@ -34,35 +36,38 @@ const getHeightAtPoint = (point: Point, map: MapType): number => {
   return charAtPoint !== '.' ? +charAtPoint : -1;
 };
 
-const walkPath = (
-  position: Point,
-  map: MapType,
-  height: number = 0,
-  ninesVisitedThisRun = new Set<string>()
-): Set<string> => {
-  // We need to check all four directions, if there is a nine, and if so add it to the total this trail can visit
-
-  const prefix = '-'.repeat(height + 1);
-  const newHeight = height + 1;
-
-  for (const dir of DIRECTIONS) {
-    const newPosition = addPoints(position, dir);
-    const isInBounds = inBounds(newPosition, map);
-
-    if (isInBounds && getHeightAtPoint(newPosition, map) === newHeight) {
-      if (newHeight === 9) {
-        ninesVisitedThisRun.add(`${newPosition.y}|${newPosition.x}`);
-      } else {
-        const visitedNines = walkPath(newPosition, map, newHeight, ninesVisitedThisRun);
-        ninesVisitedThisRun = new Set<string>([...ninesVisitedThisRun, ...visitedNines]);
-      }
-    }
-  }
-
-  return ninesVisitedThisRun;
+const pointToIndex = (point: Point, map: MapType): number => {
+  return point.x + point.y * map[point.y].length;
 };
 
 export const partOne = (input: InputType): number => {
+  const walkPath = (
+    position: Point,
+    map: MapType,
+    height: number = 0,
+    ninesVisitedThisRun = new Set<string>()
+  ): Set<string> => {
+    // We need to check all four directions, if there is a nine, and if so add it to the total this trail can visit
+
+    const newHeight = height + 1;
+
+    for (const dir of DIRECTIONS) {
+      const newPosition = addPoints(position, dir);
+      const isInBounds = inBounds(newPosition, map);
+
+      if (isInBounds && getHeightAtPoint(newPosition, map) === newHeight) {
+        if (newHeight === 9) {
+          ninesVisitedThisRun.add(`${newPosition.y}|${newPosition.x}`);
+        } else {
+          const visitedNines = walkPath(newPosition, map, newHeight, ninesVisitedThisRun);
+          ninesVisitedThisRun = new Set<string>([...ninesVisitedThisRun, ...visitedNines]);
+        }
+      }
+    }
+
+    return ninesVisitedThisRun;
+  };
+
   const map: MapType = input.map((line) => line.split(''));
   let totalTrailHeads = 0;
 
@@ -81,5 +86,51 @@ export const partOne = (input: InputType): number => {
 };
 
 export const partTwo = (input: InputType): number => {
-  return -1;
+  0;
+  const walkPath = (
+    position: Point,
+    map: MapType,
+    height: number = 0,
+    pathUntilNow: number[] = [],
+    completePaths: Set<string> = new Set<string>()
+  ) => {
+    const newHeight = height + 1;
+    const prefix = '-'.repeat(newHeight);
+
+    for (const dir of DIRECTIONS) {
+      const newPosition = addPoints(position, dir);
+      const isInBounds = inBounds(newPosition, map);
+
+      if (isInBounds && getHeightAtPoint(newPosition, map) === newHeight) {
+        pathUntilNow.push(pointToIndex(newPosition, map));
+
+        if (newHeight === 9) {
+          completePaths.add(pathUntilNow.join('_'));
+        } else {
+          completePaths = new Set([
+            ...completePaths,
+            ...walkPath(newPosition, map, newHeight, pathUntilNow, completePaths),
+          ]);
+        }
+      }
+    }
+    return completePaths;
+  };
+
+  const map: MapType = input.map((line) => line.split(''));
+
+  let totalTrailHeads = 0;
+
+  // Goto through the map and find 0's as starting points for trails and check how many '9's we can reach
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      if (map[y][x] === '0') {
+        const currentPosition: Point = { y, x };
+        const possiblePaths = walkPath(currentPosition, map, 0, [pointToIndex(currentPosition, map)]);
+        totalTrailHeads += possiblePaths.size;
+      }
+    }
+  }
+
+  return totalTrailHeads;
 };
