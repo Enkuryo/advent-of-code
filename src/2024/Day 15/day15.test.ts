9141bf4443beed82e2f6dacfa3726c44283e020b
Enkuryo <info@enkuryo.eu>
2024-12-15 17:43:31 +0100

    Add solution for 2024 day 15 part 2

--- src/2024/Day 15/day15.test.ts
+++ src/2024/Day 15/day15.test.ts
@@ -4,25 +4,46 @@ import { partOne, partTwo } from './day15';
 
 const EXAMPLE_INPUT = readFile(__dirname + '/example.txt');
 
-const SMALL_EXAMPLE_INPUT = `########
-#..O.O.#
-##@.O..#
-#...O..#
-#.#.O..#
-#...O..#
-#......#
-########
+const EDGE_CASE_1 = `#######
+#.....#
+#.OO@.#
+#.....#
+#######
 
-<^^>>>vv<v>>v<<`.split('\n');
+<<`.split('\n');
+
+const EDGE_CASE_2 = `#######
+#.....#
+#.O#..#
+#..O@.#
+#.....#
+#######
+
+<v<<^`.split('\n');
+
+const EDGE_CASE_3 = `#######
+#.....#
+#.O.O@#
+#..O..#
+#..O..#
+#.....#
+#######
+
+<v<<>vv<^^`.split('\n');
 
 describe('Year 2024 - Day 15', () => {
-  test('Part 1 - Small Example Input returns 2028', () => {
+  /*test('Part 1 - Small Example Input returns 2028', () => {
     expect(partOne(SMALL_EXAMPLE_INPUT)).toBe(2028);
   });
   test('Part 1 - Example Input returns 10092', () => {
     expect(partOne(EXAMPLE_INPUT)).toBe(10092);
+  });*/
+  test('Part 2 - Edge Cases', () => {
+    //expect(partTwo(EDGE_CASE_1)).toBe(406);
+    //expect(partTwo(EDGE_CASE_2)).toBe(509);
+    expect(partTwo(EDGE_CASE_3)).toBe(822);
   });
-  /*test('Part 2 - Example Input returns 1', () => {
-    expect(partTwo(EXAMPLE_INPUT)).toBe(1);
+  /*test('Part 2 - Example Input returns 9021', () => {
+    expect(partTwo(EXAMPLE_INPUT)).toBe(9021);
   });*/
 });
--- src/2024/Day 15/day15.ts
+++ src/2024/Day 15/day15.ts
@@ -5,6 +5,10 @@ export const PUZZLE_INPUT = readFile(__dirname + '/input.txt');
 
 type DirectionType = '<' | '^' | '>' | 'v';
 
+type RobotType = {
+  position: Point;
+};
+
 const MOVES = {
   '<': new Point(0, -1),
   '^': new Point(-1, 0),
@@ -12,10 +16,124 @@ const MOVES = {
   v: new Point(1, 0),
 };
 
-const printMap = (map: Array<Array<string>>, robot: { position: Point }): void => {
+const moveAndPush = (position: Point, move: DirectionType, map: string[][], simulate: boolean = false): boolean => {
+  const targetPosition = Point.addTwoPoints(MOVES[move], position);
+  const mapAtTarget = map[targetPosition.getY()][targetPosition.getX()];
+
+  const currentBox = map[position.getY()][position.getX()];
+
+  switch (mapAtTarget) {
+    case '#':
+      return false;
+    case 'O':
+      if (moveAndPush(targetPosition, move, map)) {
+        const boxTarget = Point.addTwoPoints(MOVES[move], targetPosition);
+        map[boxTarget.getY()][boxTarget.getX()] = 'O';
+        map[targetPosition.getY()][targetPosition.getX()] = '.';
+        return true;
+      }
+      return false;
+    case '[':
+      // Left and right behave like before
+      if (move === '>' || move === '<') {
+        if (moveAndPush(targetPosition, move, map)) {
+          const boxTarget = Point.addTwoPoints(MOVES[move], targetPosition);
+          map[boxTarget.getY()][boxTarget.getX()] = '[';
+          map[targetPosition.getY()][targetPosition.getX()] = '.';
+          return true;
+        }
+      }
+      if (move === '^' || move === 'v') {
+        if (
+          moveAndPush(targetPosition, move, map, true) &&
+          moveAndPush(Point.addTwoPoints(targetPosition, new Point(0, 1)), move, map, true)
+        ) {
+          if (!simulate) {
+            moveAndPush(targetPosition, move, map);
+            moveAndPush(Point.addTwoPoints(targetPosition, new Point(0, 1)), move, map);
+            const boxTarget = Point.addTwoPoints(MOVES[move], targetPosition);
+            map[boxTarget.getY()][boxTarget.getX()] = '[';
+            map[targetPosition.getY()][targetPosition.getX()] = '.';
+
+            const boxRightSide = Point.addTwoPoints(boxTarget, new Point(0, 1));
+            const targetRightSide = Point.addTwoPoints(targetPosition, new Point(0, 1));
+            map[boxRightSide.getY()][boxRightSide.getX()] = ']';
+            map[targetRightSide.getY()][targetRightSide.getX()] = '.';
+          }
+          return true;
+        }
+      }
+      return false;
+    case ']':
+      // Left and right behave like before
+      if (move === '>' || move === '<') {
+        if (moveAndPush(targetPosition, move, map)) {
+          const boxTarget = Point.addTwoPoints(MOVES[move], targetPosition);
+          map[boxTarget.getY()][boxTarget.getX()] = ']';
+          map[targetPosition.getY()][targetPosition.getX()] = '.';
+          return true;
+        }
+      }
+      if (move === '^' || move === 'v') {
+        if (
+          moveAndPush(targetPosition, move, map, true) &&
+          moveAndPush(Point.addTwoPoints(targetPosition, new Point(0, -1)), move, map, true)
+        ) {
+          if (!simulate) {
+            moveAndPush(targetPosition, move, map);
+            moveAndPush(Point.addTwoPoints(targetPosition, new Point(0, -1)), move, map);
+            const boxTarget = Point.addTwoPoints(MOVES[move], targetPosition);
+            map[boxTarget.getY()][boxTarget.getX()] = ']';
+            map[targetPosition.getY()][targetPosition.getX()] = '.';
+
+            const boxLeftSide = Point.addTwoPoints(boxTarget, new Point(0, -1));
+            const targetLeftSide = Point.addTwoPoints(targetPosition, new Point(0, -1));
+            map[boxLeftSide.getY()][boxLeftSide.getX()] = '[';
+            map[targetLeftSide.getY()][targetLeftSide.getX()] = '.';
+          }
+
+          return true;
+        }
+      }
+      return false;
+    case '.':
+      return true;
+  }
+
+  return false;
+};
+
+const step = (move: DirectionType, robot: RobotType, map: string[][]) => {
+  const targetPosition = Point.addTwoPoints(MOVES[move], robot.position);
+
+  if (moveAndPush(robot.position, move, map)) {
+    robot.position = targetPosition;
+  }
+};
+
+const printMap = (map: Array<Array<string>>, robot: { position: Point }, replaceWithEmoji: boolean = false): void => {
   const tmpMap: Array<Array<string>> = JSON.parse(JSON.stringify(map));
   tmpMap[robot.position.getY()][robot.position.getX()] = '@';
-  console.log(tmpMap.map((line) => line.join('')).join('\n'));
+  console.log(
+    tmpMap
+      .map((line) =>
+        line
+          .map((c) => {
+            if (replaceWithEmoji)
+              return {
+                '.': '  ',
+                '#': '🧱',
+                '@': '👨',
+                O: '📦',
+                '[': '[=',
+                ']': '=]',
+              }[c];
+            return c;
+          })
+          .join('')
+      )
+      .join('\n')
+  );
   console.log();
 };
 
@@ -23,7 +141,7 @@ export const partOne = (input: InputType): number => {
   const map: string[][] = [];
   const moveList: DirectionType[] = [];
 
-  const robot = {
+  const robot: RobotType = {
     position: new Point(),
   };
 
@@ -42,39 +160,8 @@ export const partOne = (input: InputType): number => {
   }
 
   /* Map and robot are prepared! Let's-a go! */
-
-  const moveAndPush = (position: Point, move: DirectionType): boolean => {
-    const targetPosition = Point.addTwoPoints(MOVES[move], position);
-    const mapAtTarget = map[targetPosition.getY()][targetPosition.getX()];
-
-    switch (mapAtTarget) {
-      case '#':
-        return false;
-      case 'O':
-        let canMove = moveAndPush(targetPosition, move);
-        if (canMove) {
-          const boxTarget = Point.addTwoPoints(MOVES[move], targetPosition);
-          map[boxTarget.getY()][boxTarget.getX()] = 'O';
-          map[targetPosition.getY()][targetPosition.getX()] = '.';
-        }
-        return canMove;
-      case '.':
-        return true;
-    }
-
-    return false;
-  };
-
-  const step = (move: DirectionType) => {
-    const targetPosition = Point.addTwoPoints(MOVES[move], robot.position);
-
-    if (moveAndPush(robot.position, move)) {
-      robot.position = targetPosition;
-    }
-  };
-
   for (let move of moveList) {
-    step(move);
+    step(move, robot, map);
   }
 
   let gpsSum = 0;
@@ -86,9 +173,61 @@ export const partOne = (input: InputType): number => {
     }
   }
 
+  printMap(map, robot);
+
   return gpsSum;
 };
 
-export const partTwo = (input: InputType): number => {
-  return -1;
+export const partTwo = (input: InputType, takeMapAsIs: boolean = false): number => {
+  let map: string[][] = [];
+  const moveList: DirectionType[] = [];
+
+  const robot = {
+    position: new Point(),
+  };
+
+  for (let line of input) {
+    if (line.indexOf('#') > -1) {
+      if (!takeMapAsIs)
+        line = line.replaceAll('#', '##').replaceAll('O', '[]').replaceAll('.', '..').replace('@', '@.');
+      map.push(line.split(''));
+    } else if (line === '\n') {
+    } else {
+      moveList.push(...line.split('').map((n) => n as DirectionType));
+    }
+  }
+
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      if (map[y][x] === '@') {
+        robot.position = new Point(y, x);
+        map[y][x] = '.';
+      }
+    }
+  }
+
+  /* Map and robot are prepared! Let's-a go! */
+
+  console.log('INITIAL MAP');
+  printMap(map, robot);
+
+  for (let move of moveList) {
+    step(move, robot, map);
+  }
+
+  console.log('MOVES DONE');
+  printMap(map, robot);
+
+  let gpsSum = 0;
+
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      if (map[y][x] === '[') {
+        const sum = y * 100 + x;
+        gpsSum += sum;
+      }
+    }
+  }
+
+  return gpsSum;
 };
