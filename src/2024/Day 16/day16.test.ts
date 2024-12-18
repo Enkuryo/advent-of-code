2ae22290340b368546c94736c7bd1acf2b311ed5
Enkuryo <info@enkuryo.eu>
2024-12-18 17:07:21 +0100

    Add solution for 2024 day 16 part 2

--- src/2024/Day 16/day16.test.ts
+++ src/2024/Day 16/day16.test.ts
@@ -10,14 +10,35 @@ const SMALL_EXAMPLE = `#####
 #S..#
 #####`.split('\n');
 
+const SECOND_EXAMPLE = `#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################`.split('\n');
+
 describe('Year 2024 - Day 16', () => {
-  /*test('Part 1 - Example Input returns 1004', () => {
+  test('Part 1 - Example Input returns 1004', () => {
     expect(partOne(SMALL_EXAMPLE)).toBe(1004);
-  });*/
+  });
   test('Part 1 - Example Input returns 7036', () => {
     expect(partOne(EXAMPLE_INPUT)).toBe(7036);
   });
-  /*test('Part 2 - Example Input returns 1', () => {
-    expect(partTwo(EXAMPLE_INPUT)).toBe(1);
-  });*/
+  test('Part 2 - Example Input returns 45', () => {
+    expect(partTwo(EXAMPLE_INPUT)).toBe(45);
+  });
+  test('Part 2 - Second Example Input returns 64', () => {
+    expect(partTwo(SECOND_EXAMPLE)).toBe(64);
+  });
 });
--- src/2024/Day 16/day16.ts
+++ src/2024/Day 16/day16.ts
@@ -1,87 +1,135 @@
-import { stringWidth } from 'bun';
 import Point from '../../lib/Point';
 import { readFile, type InputType } from '../../utils/readFile';
 
 export const PUZZLE_INPUT = readFile(__dirname + '/input.txt');
 
 type Cell = {
-  direction: string; // Use 'o' as placeholder for no direction
-  value: number;
+  x: number;
+  y: number;
+  path: string[];
+  cost: number;
+  direction: string;
 };
 
-const Directions: { [key: string]: [string, string, string] } = {
-  '^': ['<', '^', '>'],
-  '>': ['^', '>', 'v'],
-  v: ['>', 'v', '<'],
-  '<': ['v', '<', '^'],
+type Map = string[][];
+
+const Directions: { [key: string]: Point } = {
+  '^': new Point(-1, 0),
+  '>': new Point(0, 1),
+  v: new Point(1, 0),
+  '<': new Point(0, -1),
 };
 
-// All neighbors to check for the current tile
-const Neighbors: { [key: string]: [Point, Point, Point] } = {
-  '^': [new Point(0, -1), new Point(-1, 0), new Point(0, 1)],
-  '>': [new Point(-1, 0), new Point(0, 1), new Point(1, 0)],
-  v: [new Point(0, 1), new Point(1, 0), new Point(0, -1)],
-  '<': [new Point(1, 0), new Point(0, -1), new Point(-1, 0)],
-};
-
-export const partOne = (input: InputType): number => {
-  const map: string[][] = input.map((line) => line.split(''));
-  const cells: { [key: string]: Cell } = {};
-
-  let endPosition: string = '';
-  const cellsToProcess: string[] = [];
+const findOnMap = (map: Map, needle: string): Point => {
+  let point: Point = new Point();
 
   for (let y = 0; y < map.length; y++) {
-    for (let x = 0; x < map[0].length; x++) {
-      switch (map[y][x]) {
-        case '#':
-          break;
-        case 'E':
-          endPosition = new Point(y, x).toString();
-          cells[new Point(y, x).toString()] = {
-            direction: 'E',
-            value: Number.MAX_SAFE_INTEGER,
-          };
-          break;
-        case '.':
-        case 'S':
-          let cell: Cell = {
-            direction: map[y][x] === 'S' ? '>' : 'o',
-            value: map[y][x] === 'S' ? 0 : Number.MAX_SAFE_INTEGER,
-          };
-          if (map[y][x] === 'S') {
-            cellsToProcess.push(new Point(y, x).toString());
-          }
-          cells[new Point(y, x).toString()] = cell;
-          break;
+    for (let x = 0; x < map[y].length; x++) {
+      if (map[y][x] === needle) {
+        point.setPoint(y, x);
+        return point;
       }
     }
   }
 
-  while (cellsToProcess.length > 0) {
-    const currentProcessedCellPosition = cellsToProcess.shift() ?? '0|0';
-    const currentProcessedCell = cells[currentProcessedCellPosition];
-
-    for (let i = 0; i < 3; i++) {
-      const neighborPosition = Point.addPointToPointString(
-        currentProcessedCellPosition,
-        Neighbors[currentProcessedCell.direction][i]
-      );
-      const neighborCell = cells[neighborPosition];
-      if (neighborCell) {
-        if (currentProcessedCell.value + (i == 1 ? 1 : 1001) < neighborCell.value) {
-          neighborCell.value = currentProcessedCell.value + (i == 1 ? 1 : 1001);
-          neighborCell.direction = Directions[currentProcessedCell.direction][i];
-
-          cellsToProcess.push(neighborPosition);
-        }
-      }
-    }
-  }
-
-  return cells[endPosition].value;
+  return point;
 };
 
-export const partTwo = (input: InputType): number => {
-  return -1;
+const findPaths = (map: Map): { minCost: number; paths: string[][] } => {
+  const visited: { [key: string]: number } = {};
+  const start: Point = findOnMap(map, 'S');
+  const end: Point = findOnMap(map, 'E');
+
+  let paths: string[][] = [];
+  let minCost = Number.MAX_SAFE_INTEGER;
+
+  const cellsToCheck: Cell[] = [];
+
+  cellsToCheck.push({
+    x: start.getX(),
+    y: start.getY(),
+    cost: 0,
+    path: [],
+    direction: '>',
+  });
+
+  let i = 1;
+
+  while (cellsToCheck.length) {
+    // Get the next cell to check
+    const currentCell = cellsToCheck.shift();
+    if (!currentCell)
+      throw Error('WTF? cellsToCheck.length > 0 but no cell shifted?');
+
+    const { x, y, direction } = currentCell;
+    const key = `${x},${y},${direction}`;
+    currentCell.path.push(key);
+
+    if (currentCell.y === end.getY() && currentCell.x === end.getX()) {
+      if (currentCell.cost < minCost) {
+        paths = [currentCell.path];
+        minCost = currentCell.cost;
+      }
+      if (currentCell.cost === minCost) paths.push(currentCell.path);
+      continue;
+    }
+
+    if (!(key in visited)) visited[key] = Number.MAX_SAFE_INTEGER;
+    if (visited[key] < currentCell.cost) continue;
+    visited[key] = currentCell.cost;
+    if (currentCell.cost > minCost) continue;
+
+    Object.entries(Directions).forEach(([dir, point]) => {
+      if (
+        (dir === '^' && direction === 'v') ||
+        (dir === 'v' && direction === '^') ||
+        (dir === '<' && direction === '>') ||
+        (dir === '>' && direction === '<')
+      )
+        return;
+
+      const nx = x + point.getX();
+      const ny = y + point.getY();
+      if (map[ny][nx] === '#') return;
+      const isTurn = dir !== direction;
+      cellsToCheck.push({
+        x: nx,
+        y: ny,
+        cost: isTurn ? currentCell.cost + 1001 : currentCell.cost + 1,
+        direction: dir,
+        path: [...currentCell.path],
+      });
+    });
+  }
+
+  return {
+    minCost,
+    paths,
+  };
+};
+
+export const partOne = (input: string[]): number => {
+  const map: string[][] = input.map((line) => line.split(''));
+
+  const { minCost, paths } = findPaths(map);
+
+  return minCost;
+};
+
+export const partTwo = (input: string[]): number => {
+  const map: string[][] = input.map((line) => line.split(''));
+
+  const { minCost, paths } = findPaths(map);
+
+  const cellsInPaths: Set<string> = new Set<string>();
+
+  paths.forEach((path) =>
+    path.forEach((p) => {
+      const parts = p.split(',');
+      const newKey = parts.slice(0, 2).join(',');
+      cellsInPaths.add(newKey);
+    })
+  );
+
+  return cellsInPaths.size;
 };
