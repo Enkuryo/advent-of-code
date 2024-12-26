defb312196e959ab5cc2a406a3c938ea25708532
Enkuryo <info@enkuryo.eu>
2024-12-26 03:07:48 +0100

    Add solution for 2024 day 24 part 2

--- src/2024/Day 24/day24.test.ts
+++ src/2024/Day 24/day24.test.ts
@@ -4,17 +4,19 @@ import { partOne, partTwo } from './day24';
 
 const EXAMPLE_INPUT = readFile(__dirname + '/example.txt');
 const EXAMPLE_2_INPUT = readFile(__dirname + '/example_2.txt');
+const EXAMPLE_3_INPUT = readFile(__dirname + '/example_3.txt');
 
-console.clear();
+//console.clear();
 
 describe('Year 2024 - Day 24', () => {
-  test('Part 1 - Example Input returns 4', () => {
+  /*test('Part 1 - Example Input returns 4', () => {
     expect(partOne(EXAMPLE_INPUT)).toBe(4);
   });
   test('Part 1 - Example 2 Input returns 2024', () => {
     expect(partOne(EXAMPLE_2_INPUT)).toBe(2024);
-  });
-  /*test('Part 2 - Example Input returns 1', () => {
-    expect(partTwo(EXAMPLE_INPUT)).toBe(1);
   });*/
+
+  test("Part 2 - Example Input returns 'z00,z01,z02,z05'", () => {
+    expect(partTwo(EXAMPLE_3_INPUT)).toBe('z00,z01,z02,z05');
+  });
 });
--- src/2024/Day 24/day24.ts
+++ src/2024/Day 24/day24.ts
@@ -2,84 +2,144 @@ import { readFile, type InputType } from '../../utils/readFile';
 
 export const PUZZLE_INPUT = readFile(__dirname + '/input.txt');
 
-type RegisterType = { [key: string]: number };
-type CalculationType = { input1: string; input2: string; ops: string; output: string };
+type Operation = { (vals: boolean[]): boolean };
 
-const AND = (n1: number, n2: number): number => n1 & n2;
-const OR = (n1: number, n2: number): number => n1 | n2;
-const XOR = (n1: number, n2: number): number => n1 ^ n2;
+interface Node {
+  inputs: string[];
+  operation: string;
+}
 
-const parseInput = (input: InputType): { registers: RegisterType; calculations: CalculationType[] } => {
-  const registers: RegisterType = {};
-  const calculations: CalculationType[] = [];
+type Graph = Map<string, Node>;
 
-  for (let line of input) {
-    if (line.match(/^[yx]\d+: \d+$/)) {
-      let parts = line.split(': ');
-      registers[parts[0]] = +parts[1];
-    } else if (line !== '') {
-      const [_, input1, ops, input2, output] = line.match(/([\w\d]+) (AND|XOR|OR) ([\w\d]+) -> ([\w\d]+)/) ?? [];
-      calculations.push({
-        input1,
-        input2,
-        ops,
-        output,
-      });
-    }
-  }
+const operations = new Map<string, Operation>([
+  ['OR', (vals) => vals[0] || vals[1]],
+  ['AND', (vals) => vals[0] && vals[1]],
+  ['XOR', (vals) => vals[0] !== vals[1]],
+  ['TRUE', (_) => true],
+  ['FALSE', (_) => false],
+]);
 
-  return {
-    registers,
-    calculations,
-  };
+const parseInput = (input: string) => {
+  const [inputText, wiringText] = input.split('\n\n');
+
+  const wiring = new Map(
+    wiringText
+      .split('\n')
+      .filter((x) => x != '')
+      .map((line) => {
+        const elems = line.split(' ');
+        return [elems[4], { inputs: [elems[0], elems[2]], operation: elems[1] }];
+      })
+  );
+
+  inputText.split('\n').forEach((line) => {
+    const input = line.split(': ');
+    if (input[1] === '1') wiring.set(input[0], { inputs: [], operation: 'TRUE' });
+    if (input[1] === '0') wiring.set(input[0], { inputs: [], operation: 'FALSE' });
+  });
+
+  return wiring;
 };
 
-const calculate = (registers: RegisterType, calculations: CalculationType[]): RegisterType => {
-  while (calculations.length > 0) {
-    let nextCalculation = calculations.shift();
-    if (!nextCalculation) throw 'How?';
+const evaluate = (nodeName: string, wiring: Graph): boolean => {
+  let node = wiring.get(nodeName)!;
+  if (node.inputs.length === 0) return operations.get(node.operation)!([]);
 
-    // If not both inputs exists, push the calculation back to the stack
-    if (registers[nextCalculation.input1] === undefined || registers[nextCalculation.input2] === undefined) {
-      calculations.push(nextCalculation);
-      continue;
-    }
+  // // copy the wiring so we don't mess everything up
+  // wiring = new Map([...wiring].map(([name, node]) => [name, {inputs: node.inputs.slice(), operation: node.operation}]));
 
-    const input1 = registers[nextCalculation.input1];
-    const input2 = registers[nextCalculation.input2];
-
-    switch (nextCalculation.ops) {
-      case 'AND':
-        registers[nextCalculation.output] = AND(input1, input2);
-        break;
-      case 'OR':
-        registers[nextCalculation.output] = OR(input1, input2);
-        break;
-      case 'XOR':
-        registers[nextCalculation.output] = XOR(input1, input2);
-        break;
-    }
-  }
-
-  return registers;
+  // // get the copied node
+  // node = wiring.get(nodeName)!;
+  const inputs = node.inputs.map((node) => evaluate(node, wiring));
+  const value = operations.get(node.operation)!(inputs);
+  node.inputs = [];
+  node.operation = value ? 'TRUE' : 'FALSE';
+  return value;
 };
 
-const getResult = (registers: RegisterType): number => {
-  let result = Object.keys(registers)
-    .filter((key) => /^z\d+$/.test(key))
-    .sort((a, b) => a.localeCompare(b))
-    .reverse()
-    .map((key) => registers[key])
-    .join('');
-  return parseInt(result, 2);
-};
+const convertToDecimal = (nodes: string[], wiring: Graph): number =>
+  [...nodes.map((nodeName) => [nodeName, evaluate(nodeName, wiring)] as [string, boolean])]
+    .sort((lhs, rhs) => lhs[0].localeCompare(rhs[0]))
+    .map(([name, val], idx) => (val ? 2 ** idx : undefined))
+    .filter((x) => x !== undefined)
+    .reduce((lhs, rhs) => lhs + rhs);
+
+const getDecimalValueOfNodesStartingWith = (c: string, wiring: Graph): number =>
+  convertToDecimal([...wiring.keys().filter((node) => node[0] === c)], wiring);
 
 export const partOne = (input: InputType): number => {
-  let { registers, calculations } = parseInput(input);
-  registers = calculate(registers, calculations);
-  return getResult(registers);
+  const wiring = parseInput(input.join('\n'));
+  return getDecimalValueOfNodesStartingWith('z', wiring);
 };
 
-export const partTwo = (input: InputType): number => {
-  return -1;
+export const partTwo = (input: InputType): string => {
+  const wiring = parseInput(input.join('\n'));
+  const swaps = new Set<string>();
+
+  const getNodeName = (inputs: string[], operation: string): string | undefined => {
+    const entry = wiring
+      .entries()
+      .find(([_, node]) => node.operation === operation && inputs.every((input) => node.inputs.includes(input)));
+    return entry ? entry[0] : undefined;
+  };
+
+  const swap = (lhs: string, rhs: string) => {
+    const temp = wiring.get(lhs);
+    wiring.set(lhs, wiring.get(rhs)!);
+    wiring.set(rhs, temp!);
+    swaps.add(lhs);
+    swaps.add(rhs);
+  };
+
+  let carryIn: string | undefined = undefined;
+  let carryOut: string | undefined = undefined;
+
+  for (const number of Array(45).keys()) {
+    const label = number.toString().padStart(2, '0');
+
+    // *** half adder ***
+    // x__ XOR y__ -> node0
+    // x__ AND y__ -> node1
+    // carryIn AND node0 -> node2
+    // carryIn XOR node0 -> output
+    // node1 OR node2 -> carry1
+    let node0 = getNodeName(['x' + label, 'y' + label], 'XOR')!;
+    let node1 = getNodeName(['x' + label, 'y' + label], 'AND')!;
+    let output: string = '';
+
+    if (carryIn !== undefined) {
+      let node2 = getNodeName([carryIn, node0], 'AND');
+      if (node2 === undefined) {
+        swap(node0, node1);
+        [node0, node1] = [node1, node0];
+        node2 = getNodeName([carryIn, node0], 'AND')!;
+      }
+
+      output = getNodeName([carryIn, node0], 'XOR')!;
+      if (node0?.startsWith('z')) {
+        swap(node0, output!);
+        [output, node0] = [node0, output];
+      }
+      if (node1?.startsWith('z')) {
+        swap(node1, output!);
+        [output, node1] = [node1, output];
+      }
+      if (node2?.startsWith('z')) {
+        swap(node2, output!);
+        [output, node2] = [node2, output];
+      }
+
+      carryOut = getNodeName([node1, node2], 'OR');
+    }
+
+    if (carryOut?.startsWith('z') && carryOut != 'z45') {
+      swap(carryOut, output!);
+      [carryOut, output] = [output, carryOut];
+    }
+
+    if (carryIn) carryIn = carryOut;
+    else carryIn = node1;
+  }
+
+  return [...swaps].sort().join(',');
 };
