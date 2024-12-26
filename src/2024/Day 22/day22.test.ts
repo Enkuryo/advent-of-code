0b60a925a3c8700c5b3fb88fbc2a09e4fc4a1eec
Enkuryo <info@enkuryo.eu>
2024-12-26 01:22:41 +0100

    Add solution for 2024 day 22 part 2

--- src/2024/Day 22/day22.test.ts
+++ src/2024/Day 22/day22.test.ts
@@ -3,14 +3,23 @@ import { readFile } from '../../utils/readFile';
 import { partOne, partTwo } from './day22';
 
 const EXAMPLE_INPUT = readFile(__dirname + '/example.txt');
+const EXAMPLE_2_INPUT = readFile(__dirname + '/example_2.txt');
+const EXAMPLE_SHORT_INPUT = readFile(__dirname + '/example_short.txt');
 
 console.clear();
 
 describe('Year 2024 - Day 22', () => {
-  test('Part 1 - Example Input returns 37327623', () => {
+  /*test('Part 1 - Example Input returns 37327623', () => {
     expect(partOne(EXAMPLE_INPUT)).toBe(37327623);
-  });
-  /*test('Part 2 - Example Input returns 1', () => {
-    expect(partTwo(EXAMPLE_INPUT)).toBe(1);
   });*/
+
+  /*test('Part 2 - Example Input returns 23', () => {
+    expect(partTwo(EXAMPLE_INPUT)).toBe(23);
+  });*/
+  /*test('Part 2 - Example Input returns 6', () => {
+    expect(partTwo(EXAMPLE_SHORT_INPUT, 10)).toBe(6);
+  });*/
+  test('Part 2 - Example Input returns 23', () => {
+    expect(partTwo(EXAMPLE_2_INPUT)).toBe(23);
+  });
 });
--- src/2024/Day 22/day22.ts
+++ src/2024/Day 22/day22.ts
@@ -2,6 +2,8 @@ import { readFile, type InputType } from '../../utils/readFile';
 
 export const PUZZLE_INPUT = readFile(__dirname + '/input.txt');
 
+type BuyerType = { secret: number; price: number; diff: number }[];
+
 const evole = (secret: number) => {
   let currentSecret: number = secret;
 
@@ -34,20 +36,60 @@ const prune = (secret: number): number => {
   return secret % 16777216;
 };
 
-export const partOne = (input: InputType): number => {
-  let sum = 0;
-
-  for (let i of input) {
-    let sSum = 0;
-    let s = +i;
-    for (let j = 0; j < 2000; j++) {
-      s = evole(s);
-    }
-    sum += s;
-  }
-  return sum;
+const iterate = (secret: number, steps: number): number => {
+  return Array.from({ length: steps }).reduce((s: number) => evole(s), secret);
 };
 
-export const partTwo = (input: InputType): number => {
-  return -1;
+const sequence = (secret: number, steps: number): number[][] => {
+  const results: number[][] = [];
+  let s = secret;
+  let prev = secret % 10;
+
+  for (let i = 0; i < steps; i++) {
+    s = evole(s);
+    results.push([s % 10, (s % 10) - prev]);
+    prev = s % 10;
+  }
+
+  return results;
+};
+
+const seqToString = (sequence: number[]): string => {
+  return sequence.join(',');
+};
+
+export const partOne = (input: InputType): number => {
+  return input.map((i) => iterate(+i, 2000)).reduce((a, b) => a + b, 0);
+};
+
+export const partTwo = (input: InputType, steps: number = 2000): number => {
+  let totalBananas: { [key: string]: number } = {};
+
+  input.forEach((secret) => {
+    const changes = sequence(+secret, 2000);
+    const currentBananasForSequence: { [key: string]: number } = {};
+
+    let currentSequence = changes.slice(0, 4).map((d) => d[1]);
+    currentBananasForSequence[seqToString(currentSequence)] = changes[3][0];
+
+    for (let i = 4; i < changes.length; i++) {
+      currentSequence.push(changes[i][1]);
+      currentSequence.shift();
+
+      const seqKey = seqToString(currentSequence);
+      if (currentBananasForSequence[seqKey] === undefined) {
+        currentBananasForSequence[seqKey] = changes[i][0];
+      }
+    }
+
+    // Merge
+    for (let seq in currentBananasForSequence) {
+      if (totalBananas[seq] === undefined) {
+        totalBananas[seq] = 0;
+      }
+      totalBananas[seq] += currentBananasForSequence[seq];
+    }
+  });
+
+  return Math.max(...Object.values(totalBananas));
 };
